cd0659c892e36fcc7d81ff1680d426a6b628ebe1
Mumtaz Khan <mumtzkhan7@gmail.com>
2016-11-15 23:38:57 +0000

    installed carrierwave gem

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
-class PhotoController < ApplicationController
+class PhotosController < ApplicationController
   before_action :authenticate_user!
   def create
-    @place = Place.find(params [:place_id])
+    @place = Place.find(params[:place_id])
     @place.photos.create(photo_params)
     redirect_to place_path(@place)
   end
@@ -11,5 +11,5 @@ class PhotoController < ApplicationController
   def photo_params
     params.require(:photo).permit(:picture, :caption)
   end
-  
+
 end
