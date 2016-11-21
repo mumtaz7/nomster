c7e7aefbc1190a3e3a377cefa697e0157f1217fe
Mumtaz Khan <mumtzkhan7@gmail.com>
2016-11-21 20:36:44 +0000

    added mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,3 +1,8 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomster-app.com"
+
+  def comment_added
+    mail(to: "mumtzkhan7@gmail.com",
+          subject: "A comment has been added to your place")
+  end
 end
