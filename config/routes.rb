30a94673d2013014f1e93900acb7e758db1ecd98
Mumtaz Khan <mumtzkhan7@gmail.com>
2016-11-29 04:50:41 +0000

    trying to fix users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-devise_for :users
-root 'places#index'
-resources :places do
-  resources :comments, only: :create
-  resources :photos, only: :create
-end
-devise_for :users
-resource :users, only: :show
+  devise_for :users
+  root 'places#index'
+
+  resources :places do
+    resources :comments, :only => :create
+  end
+
+  resources :places do
+    resources :photos, :only => :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
