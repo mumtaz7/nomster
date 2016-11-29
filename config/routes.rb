4fa9bf2aba0d9062396cf394a0ae18e29e2a1d93
Mumtaz Khan <mumtzkhan7@gmail.com>
2016-11-29 16:38:26 +0000

    fixed routes for user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root 'places#index'
 
   resources :places do
-    resources :comments, :only => :create
+    resources :comments, only: :create
+    resources :photos, only: :create
   end
-
-  resources :places do
-    resources :photos, :only => :create
-  end
+  
+  resources :users, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
