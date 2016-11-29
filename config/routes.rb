9e72d30eb7ff1b15e772db2ef7350bb78188a132
Mumtaz Khan <mumtzkhan7@gmail.com>
2016-11-29 04:34:49 +0000

    user page not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root 'places#index'
-
-  resources :places do
-    resources :comments, only: :create
-    resources :photos
-  end
-
-  resources :photos do
-    resources :photos, only: :create
-  end
-
-  resources :users, only: :show
+devise_for :users
+root 'places#index'
+resources :places do
+  resources :comments, only: :create
+  resources :photos, only: :create
+end
+devise_for :users
+resource :users, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
